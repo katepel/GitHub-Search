81a9b1b0eacae92ab527018f2ba258940bbdf4b8
Katarina Pelicarić <katarina.pelicaric@fer.hr>
2017-08-26 11:29:29 +0200

    Initial Commit

---
+++ GitHub Search/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  GitHub Search
+//
+//  Created by Katarina Pelicaric on 26/08/2017.
+//  Copyright © 2017 Katarina Pelicaric. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
